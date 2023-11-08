5d6eea861a16d9139c3e16c482ca24dc424f4760
justintingley <justin@bocbanking.com>
2023-11-08 15:19:21 -0600

    second

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
+const port = 4000;
 
 app.use("/", (req, res, next) => {
-    res.send("Hello World, its daddy");
+    res.send("CloudGod is at work");
 });
 
-app.listen(4000);
+mongoose.connect(
+    "mongodb+srv://admin:Tingley1234...@blogappcluster.vq6ohp7.mongodb.net/Blog?retryWrites=true&w=majority"
+    ).then(() => {
+        app.listen(port, () => {
+            console.log(`Connected to DB and Server running on port: ${port}`);
+        });
+    })
+    .catch((err) => console.log(err));
